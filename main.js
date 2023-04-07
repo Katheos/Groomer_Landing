cb4f95e22e3b04ffb878e8e4af1ecd0ec05984ed
Serhii Karpov <ie18072002ksa@gmail.com>
2023-04-07 11:47:14 +0300

    finished adaptive

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const dropdownBtn = document.getElementById('dropdown-btn');
 const dropdown = document.getElementById('dropdown');
+const burgerMenu = document.getElementById('burger_menu');
+const header = document.getElementById('header-ul');
+
 
 dropdownBtn.addEventListener('click' , function() {
     if(dropdown.style.display === 'block'){
         dropdown.style.display = 'none';
     } else {
         dropdown.style.display = 'block';
+    }
+})
+
+
+burgerMenu.addEventListener('click', function () {
+    if(header.style.display === 'flex'){
+        header.style.display = 'none';
+    } else {
+        header.style.display = 'flex';
     }
 })
